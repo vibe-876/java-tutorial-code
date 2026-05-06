77c2bb9b45aaaa87e36154092431eb2e12b785fc
cam <cameronayres123@gmail.com>
2026-05-06 10:52:30 +0100

    Stuff.

---
+++ init.el
@@ -0,0 +1,6 @@
+(cua-mode 1)
+
+(global-set-key (kbd "C-o") 'find-file)
+(global-set-key (kbd "C-b") 'switch-buffer)
+(global-set-key (kbd "<f5>") (lambda ()
+			       (compile ".\gradlew.bat runClient")))

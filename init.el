19ed2b04b4acd95f920849a925414655eddc39cb
cam <cameronayres123@gmail.com>
2026-05-06 13:03:21 +0100

    Fixed bug by removing offending line.

--- init.el
+++ init.el
@@ -2,8 +2,6 @@
 
 (global-set-key (kbd "C-o") 'find-file)
 (global-set-key (kbd "C-b") 'switch-buffer)
-(global-set-key (kbd "<f5>") (lambda ()
-			       (compile ".\gradlew.bat runClient")))
-(global-set-key (kbd "<f6>") #'compile)
+(global-set-key (kbd "<f5>") #'compile)
 
 (load-theme 'deeper-blue t)

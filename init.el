966f59b2ed57171ab4a7c784b2761995313b0aab
cam <cameronayres123@gmail.com>
2026-05-06 12:33:24 +0100

    Added keybind for compile command.

--- init.el
+++ init.el
@@ -4,5 +4,6 @@
 (global-set-key (kbd "C-b") 'switch-buffer)
 (global-set-key (kbd "<f5>") (lambda ()
 			       (compile ".\gradlew.bat runClient")))
+(global-set-key (kbd "<f6>") #'compile)
 
 (load-theme 'deeper-blue t)

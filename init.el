ded78c40a0c8534d6aced051fb605386b546175d
cam <cameronayres123@gmail.com>
2026-05-06 13:08:30 +0100

    Final tweaks.

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (cua-mode 1)
 
-(global-set-key (kbd "C-o") 'find-file)
-(global-set-key (kbd "C-b") 'switch-buffer)
+(global-set-key (kbd "C-o") #'find-file)
+(global-set-key (kbd "C-b") #'switch-buffer)
 (global-set-key (kbd "<f5>") #'compile)
 
 (load-theme 'deeper-blue t)
